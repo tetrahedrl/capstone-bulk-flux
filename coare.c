ceab5a3e53094dc75ed566f32d61354dd8d92e7b
Ry McClure <rymcclure73@gmail.com>
2022-01-26 13:20:11 -0500

    1/26 Progress

--- coare.c
+++ coare.c
@@ -15,9 +15,17 @@ double starT, starQ, starU, starUt;
 double reynoldsR, reynoldsT, reynoldsQ;
 double psiU, psiH;
 double fluxS, fluxL, fluxT;
+double prevS = 1;
+
+double stability;
+double gustiness;
+double tau;
+
+int i;
 
 double humidity(double t, double p)
 {
+    p = 1000;
     double qs = 0.98 * ((1.0007 + 0.00000356 * p) * 6.1121 * exp(17.502 * t / (240.97 + t)));
     return 0.62197 * (qs / (p - 0.378 * qs));
 }
@@ -29,7 +37,7 @@ double scalingParam(double coefTransfer, double interfaceValue, double tempDepen
 
 void calcZeta()
 {
-    zeta = measureZ * ((karman * grav / airT) * (starT + 0.61 * airT * starQ) / (starU * starU));
+    zeta = measureZ * ((karman * grav / (airT+273.)) * (starT + 0.61 * (airT+273.) * starQ) / (starU * starU));
 }
 
 void calcRoughZ()
@@ -61,7 +69,7 @@ void reynoldsConvert()
 double sqrtNeutrals(double reynolds, double correction)
 {
     double roughZEQ = (viscAir / starU) * reynolds;
-    return correction * karman / log(measureZ / roughZEQ);
+    return correction * (karman / log10(measureZ / roughZEQ));
 }
 
 double sqrtComponents(double sqrtNeutral, double psi, double correction)
@@ -83,15 +91,15 @@ int main()
 {
 
     takeInputs();
-    printf("Air T\n%lf", airT);
+    //printf("Air T\n%lf", airT);
 
     specificQ = specificQ / 1000; // 
 
     viscAir = 0.00001326 * (1 + 0.006542 * airT + 0.000008301 * (airT *airT) - 0.00000000484 * pow(airT, 3));
-    printf("\nAir Viscosity\n%lf", viscAir);
+    //printf("\nAir Viscosity\n%lf", viscAir);
 
     enthalpyL = 100000 * (25 - 0.02274 * airT);
-    printf("\nLatent Heat of Vaporization\n%lf", enthalpyL);
+    //printf("\nLatent Heat of Vaporization\n%lf", enthalpyL);
 
     windS = sqrt((windU * windU) + (wg * wg));
     //printf("\n%lf\n%lf\n%lf\n%lf\n%lf\n%lf", wg, coefEN, coefHN, coefDN, coefTN, coefQN);
@@ -100,45 +108,53 @@ int main()
     potDiffT = fabs(airT - (surfaceT + 0.0098 * measureZ));
     interfSpecificQ = humidity(airT, pressure);
 
-    printf("\nspecificQ\n%lf\ninterfSpecificQ\n%lf", specificQ, interfSpecificQ);
-    printf("\nairT\n%lf\npotT\n%lf\npotDiffT\n%lf", airT, potT, potDiffT);
+    //printf("\nspecificQ\n%lf\ninterfSpecificQ\n%lf", specificQ, interfSpecificQ);
+    //printf("\nairT\n%lf\npotT\n%lf\npotDiffT\n%lf", airT, potT, potDiffT);
 
     starU = 0.04 * windS;
-    starT = 0.04 * potDiffT;
-    starQ = fabs(specificQ - interfSpecificQ);
+    starT = -1 * 0.04 * potDiffT;
+    starQ = -1 * fabs(specificQ - interfSpecificQ);
 
-    for(int i = 0; i < 20; i++)
+    //for(int i = 0; i < 20; i++)
+    while(abs(prevS - fluxS) > .001)
     {
-        printf("\n\nairT\n%lf\nstarT\n%lf\nstarQ\n%lf\nstarU\n%lf\n", airT, starT, starQ, starU);
+        prevS = fluxS;
+        i++;
+        //printf("\n\nairT\n%lf\nstarT\n%lf\nstarQ\n%lf\nstarU\n%lf\n", airT, starT, starQ, starU);
         calcZeta();
-        printf("\n\nzeta\n%lf\n", zeta);
+        //printf("\n\nzeta\n%lf\n", zeta);
         calcRoughZ();
-        printf("\n\nroughZ\n%lf\n", roughZ);
+        //printf("\n\nroughZ\n%lf\n", roughZ);
         reynoldsR = starU * roughZ / viscAir;
         reynoldsConvert();
         //printf("\noriginal DN\n%lf", karman / log(measureZ / roughZ));
         getPsi(zeta);
         sqrtCoefDN = sqrtNeutrals(reynoldsR, 1);
-        sqrtCoefTN = sqrtNeutrals(reynoldsT, alpha);
-        sqrtCoefQN = sqrtNeutrals(reynoldsQ, alpha);
+        sqrtCoefTN = sqrtNeutrals(reynoldsT, aCorr);
+        sqrtCoefQN = sqrtNeutrals(reynoldsQ, aCorr);
         //printf("\nDN\n%lf", sqrtCoefDN);
         //printf("\nTN\n%lf", sqrtCoefTN);
         //printf("\nQN\n%lf", sqrtCoefQN);
         sqrtCoefD = sqrtComponents(sqrtCoefDN, psiU, 1);
-        sqrtCoefT = sqrtComponents(sqrtCoefTN, psiH, alpha);
-        sqrtCoefQ = sqrtComponents(sqrtCoefQN, psiH, alpha);
+        sqrtCoefT = sqrtComponents(sqrtCoefTN, psiH, aCorr);
+        sqrtCoefQ = sqrtComponents(sqrtCoefQN, psiH, aCorr);
         starUt = sqrt(sqrtCoefD * sqrtCoefD * windS * windS);
         starT = -1 * sqrtCoefT * (surfaceT - potT);
         starQ = -1 * sqrtCoefQ * (interfSpecificQ - specificQ);
         fluxS = -1 * pressure * 1004.67 * starU * starT;
         fluxL = -1 * pressure * enthalpyL * starU * starQ;
+        printf("\n\nLatent Heat Flux Old %lf", fluxL);
+        stability = 1 / ((1 - sqrtCoefDN * psiU / karman) * (1 - sqrtCoefQN * psiH / (aCorr * karman)));
+        gustiness = sqrt(1 + pow(wg / windU, 2));
+        fluxL = pressure * enthalpyL * coefEN * windU * (interfSpecificQ - specificQ) * gustiness * stability;
         fluxT = -1 * pressure * starU * starU;
+        tau = starU * starU * pressure;
 
         printf("\n\nLoop counter %d", i);
         printf("\n\nSensible Heat Flux %lf", fluxS);
         printf("\n\nLatent Heat Flux %lf", fluxL);
 
-
+        starU = starUt;
         //printf("\npsiC(zeta)\n%lf", psiC(zeta));
     }
 
